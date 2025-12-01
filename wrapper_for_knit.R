26c84dcb5610bbaa4c2491ef7a869cbb550957d2
Mark <mark.aaron.fisher@gmail.com>
2025-11-30 20:51:30 -0500

    Implement google calendar hook (#1)
    * add google calendar access automation
    
    * add env template file and fix minor bug
    
    * fix bugs in invoice_knit.Rmd to make it not rely on a latex package that is only weeks old
    
    * fix bug where it charges for last month
    
    * add option to add month and year specific parameters
    
    * do not track individual invoices
    
    * change client
    
    * remove redundant template

--- wrapper_for_knit.R
+++ wrapper_for_knit.R
@@ -1,4 +1,26 @@
-require(knitr)
-require(rmarkdown)
-require(lubridate)
-rmarkdown::render("/Users/mf/2014_and_beyond/Side_projects/jiuJitsuCoaching/invoice_knit.Rmd")
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(rmarkdown)
+})
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) < 3) {
+  stop("Usage: wrapper_for_knit.R <csv_path> <invoice_date> <output_pdf>", call. = FALSE)
+}
+
+csv_path    <- args[1]
+invoice_date <- args[2]      # e.g. "2025-08-26"
+output_pdf  <- args[3]
+
+rmarkdown::render(
+  "invoice_knit.Rmd",
+  params = list(
+    csv_path    = csv_path,
+    invoice_date = invoice_date
+  ),
+  output_format = "pdf_document",
+  output_file   = output_pdf,
+  envir = new.env()         # keep the environment clean
+)
